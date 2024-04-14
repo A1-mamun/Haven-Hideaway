8e0d357eebc9ee822e85997bcd9a9f0ba59ef184
A1-mamun <1154almamun@gmail.com>
2024-04-15 00:48:27 +0600

    Registration page design complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -12,70 +12,84 @@ const Login = () => {
     console.log(email, password);
   };
 
+  const handleLogInWithGoogle = (e) => {
+    e.preventDefault();
+    console.log("google clicked");
+  };
+  const handleLogInWithGithub = (e) => {
+    e.preventDefault();
+    console.log("github clicked");
+  };
   return (
-    <div>
-      <div className="hero min-h-full bg-base-200 py-10 rounded-xl">
-        <div className="hero-content">
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <h2 className="text-3xl text-center">Login your account</h2>
-            <form onSubmit={handleLogIn} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email address"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  className="input input-bordered"
-                  required
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control">
-                <button className="btn btn-primary">Login</button>
-              </div>
-              <div className="flex justify-between items-center">
-                <div className="border border-gray-500 h-[1px] grow"></div>
-                <h4 className="mx-2">or</h4>
-                <div className="border border-gray-500 h-[1px] grow"></div>
-              </div>
-              <div className="">
-                <button className="btn btn-outline w-full">
-                  <FcGoogle />
-                  Login with Google
-                </button>
-              </div>
-              <div className="">
-                <button className="btn btn-outline w-full">
-                  <FaGithub />
-                  Login with Github
-                </button>
-              </div>
-            </form>
-            <h2 className="text-center">
-              Do not have an Account ?
-              <Link to="/register" className="text-orange-700 font-medium">
-                Register
-              </Link>
-            </h2>
+    <div className="hero h-full bg-base-200 py-10 rounded-xl">
+      <div className="hero-content">
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <h2 className="text-3xl text-center">Login your account</h2>
+          <form onSubmit={handleLogIn} className="card-body pb-0 pt-3">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <div className="card-body pt-2">
+            <div className="flex justify-between items-center">
+              <div className="border border-gray-500 h-[1px] grow"></div>
+              <h4 className="mx-2">or</h4>
+              <div className="border border-gray-500 h-[1px] grow"></div>
+            </div>
+            <div className="">
+              <button
+                onClick={handleLogInWithGoogle}
+                className="btn btn-outline w-full"
+              >
+                <FcGoogle />
+                Login with Google
+              </button>
+            </div>
+            <div className="mt-3">
+              <button
+                onClick={handleLogInWithGithub}
+                className="btn btn-outline w-full"
+              >
+                <FaGithub />
+                Login with Github
+              </button>
+            </div>
           </div>
+          <h2 className="text-center">
+            Do not Have An Account ?
+            <Link to="/register" className="text-orange-700 font-medium">
+              Register
+            </Link>
+          </h2>
         </div>
       </div>
     </div>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const name = form.get("name");
+    const photo = form.get("photo");
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(name, photo, email, password);
+  };
+
   return (
-    <div>
-      <h2 className="text-3xl">register page</h2>
+    <div className="hero h-full py-10 rounded-xl bg-base-200">
+      <div className="hero-content ">
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <h2 className="text-3xl text-center">Register your account</h2>
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Enter your Photo URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-3">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+          <h2 className="text-center pb-5">
+            Already Have An Account ?
+            <Link to="/login" className="text-orange-700 font-medium">
+              LogIn
+            </Link>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
