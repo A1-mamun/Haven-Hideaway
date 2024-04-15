52041544a08a3e78b660ad49129a201101ece704
A1-mamun <1154almamun@gmail.com>
2024-04-15 15:49:59 +0600

    Register page password validation, show and hide password and registration error handle

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProviders";
 
 const Register = () => {
+  const [error, setError] = useState("");
+  const [succes, setSucces] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const { createUser } = useContext(AuthContext);
 
   const handleRegister = (e) => {
@@ -13,9 +17,28 @@ const Register = () => {
     const email = form.get("email");
     const password = form.get("password");
     // console.log(name, photo, email, password);
+    // reset error
+    setError("");
+    setSucces("");
+
+    // password validation
+    if (password.length < 6) {
+      setError("password should be at least 6 characters");
+      return;
+    } else if (!/^(?=.*[A-Z])/.test(password)) {
+      setError("password should have at least one Uppercase letter");
+      return;
+    } else if (!/^(?=.*[a-z])/.test(password)) {
+      setError("password should have at least one Lowercase letter");
+      return;
+    }
     createUser(email, password)
-      .then((result) => console.log(result.user))
-      .catch((error) => console.error(error));
+      .then((result) => setSucces("User created Successfully"))
+      .catch((error) => {
+        const fullError = error.message;
+        const shortError = fullError.slice(22, fullError.length - 2);
+        setError(shortError);
+      });
   };
 
   return (
@@ -60,22 +83,30 @@ const Register = () => {
                 required
               />
             </div>
-            <div className="form-control">
+            <div className="form-control relative">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="Enter your password"
                 className="input input-bordered"
                 required
               />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-[52px] right-4"
+              >
+                {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
+              </span>
             </div>
             <div className="form-control mt-3">
               <button className="btn btn-primary">Register</button>
             </div>
           </form>
+          {error && <p className="text-red-700">{error}</p>}
+          {succes && <p className="text-green-600">{succes}</p>}
           <h2 className="text-center pb-5">
             Already Have An Account ?
             <Link to="/login" className="text-orange-700 font-medium">
