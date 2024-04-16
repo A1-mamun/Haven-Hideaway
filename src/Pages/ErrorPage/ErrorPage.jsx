0973f4dbd67df0f672aa364552dee704dfcfada4
A1-mamun <1154almamun@gmail.com>
2024-04-16 15:34:38 +0600

    Eroor Page designe complete

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h2 className="text-4xl text-center">404</h2>
+      <h3 className="text-3xl my-5">Page Not Found</h3>
+      <h4 className="text-2xl mb-5">Back to Home</h4>
+      <Link to="/">
+        <button className="btn btn-error">Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import Root from "../layout/Root";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
